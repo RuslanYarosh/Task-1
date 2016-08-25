7d25ffa7d346c5374f7f5cfd7e532a6f0d35eb14
Rusik <ruslan.yarosh@bk.ru>
2016-08-25 16:58:32 +0300

    Update Task-1.rb
    Commit

--- Task-1.rb
+++ Task-1.rb
@@ -1,11 +1,11 @@
 module Tasks
-  def candies(x,y,z,w)
+  def candies(x,y,z,w) 
     if x < 0 or y < 0 or z < 0 or w<=0
       puts "Enter x,y,z>=0 w>0"
     else
-      rusik = [x,y,z].sort.reverse
-      n1 = w / rusik[0]
-      result = 0
+      rusik = [x,y,z].sort.reverse  # sort the variables in the array from high to low
+      result = 0  # number of gift options
+      n1 = w / rusik[0]  # define how much the required value is greater than the value at index 0
       0.upto(n1) do |i|
         first = (w - i*rusik[0])
         n2 = first / rusik[1]
