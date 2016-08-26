c7cab4bf33593346e2eb05cc02ef13ed0d50346d
Rusik <ruslan.yarosh@bk.ru>
2016-08-26 12:22:03 +0300

    Update Task-1.rb

--- Task-1.rb
+++ Task-1.rb
@@ -3,7 +3,7 @@ module Tasks
     if x < 0 or y < 0 or z < 0 or w<=0
       puts "Enter x,y,z>=0 w>0"
     else
-      gift = [x,y,z].sort.reverse  # sort the variables(gifts) in the array from high to low weight
+      gift = [x,y,z].sort.reverse  # sort the gifts in the array from high to low weight
       result = 0  # the desired result is set to 0
       n1 = w / gift[0]  # possible number of gifts with index 0
       0.upto(n1) do |i| # cycle from 0 to n1 gifts with index 0
